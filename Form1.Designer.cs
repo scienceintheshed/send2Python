72b0f5ddad935dffb8a96a8cada80d315e2fbb51
Steven Owen <steven@illawarracoatings.com>
2020-06-15 17:32:17 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace send2Python
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sendToPythonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(31, 54);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(50, 20);
+            this.txtValue1.TabIndex = 0;
+            this.txtValue1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(221, 54);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(50, 20);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Values";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sendToPythonToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(300, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sendToPythonToolStripMenuItem
+            // 
+            this.sendToPythonToolStripMenuItem.Name = "sendToPythonToolStripMenuItem";
+            this.sendToPythonToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.sendToPythonToolStripMenuItem.Text = "Calculate in Python";
+            this.sendToPythonToolStripMenuItem.Click += new System.EventHandler(this.sendToPythonToolStripMenuItem_Click);
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(102, 54);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(50, 20);
+            this.txtValue2.TabIndex = 9;
+            this.txtValue2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 95);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sendToPythonToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace send2Python
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+
+        private void sendToPythonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string args = string.Format(@"C:\users\steve\AppData\Local\Programs\Python\Python38\add.py {0} {1}", txtValue1.Text, txtValue2.Text);
+            string python = @"C:\users\steve\AppData\Local\Programs\Python\Python38\python.exe";
+
+            try
+            {
+                Process calc = new Process();
+                ProcessStartInfo calcProcessStartInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    FileName = python,
+                    Arguments = args
+                };
+
+                calc.StartInfo = calcProcessStartInfo;
+                calc.Start();
+
+                StreamReader myStreamReader = calc.StandardOutput;
+                string _temp = myStreamReader.ReadLine();
+                txtResult.Text = _temp;
+                calc.WaitForExit();
+            }
+            catch
+            {
+
+            }           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
